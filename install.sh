b57cc27d0921e51e8fca589ec85c5468829b7bd6
Dima Krasner <dima@dimakrasner.com>
2013-09-19 13:31:43 +0000

    Added man pages.

--- install.sh
+++ install.sh
@@ -3,8 +3,12 @@ for i in *
 do
 	install -D -m 755 $i "$1/bin/$i"
 done
-ln -s sulogin "$1/bin/su"
-ln -s sulogin "$1/bin/login"
+cd ../man
+for i in *
+do
+	category="$(echo $i | cut -f 2 -d .)"
+	install -D -m 644 $i "$1/usr/share/man/man$category/$i"
+done
 cd ..
 install -D -m 644 README "$1/usr/share/doc/lazy-utils/README"
 install -m 644 AUTHORS "$1/usr/share/doc/lazy-utils/AUTHORS"
